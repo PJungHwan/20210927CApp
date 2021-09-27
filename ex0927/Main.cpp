5fd393232feb7d2da37f549207ce13dd8764fea7
박정환 <pjh584700@naver.com>
2021-09-27 10:38:57 +0900

    Main.cpp 의 Print() 함수 완성하고 숨정함.

--- ex0927/Main.cpp
+++ ex0927/Main.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
 int main()
 {
-	printf("Hello!\n");
-	printf("Hello!\n");
-	printf("Hello!\n");
-	printf("Hello!\n");
-	printf("Hello!\n");
+	for (int i = 0; i < 10; ++i)
+		printf("Hello : %d\n", i);
 }
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//}
 
 //#include <stdio.h>
 //int main()
